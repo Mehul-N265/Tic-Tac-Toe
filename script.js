908fa568b047ffec1ffc2ab2f2bb22957114ab0c
Mehul-N265 <76092343+Mehul-N265@users.noreply.github.com>
2021-11-06 21:31:18 +0530

    Checkwin (#4)
    * Done with the UI of reset btn
    
    * Changes made in checkWinner function for more accuracy
    
    * Done with main game! Everything works fine . Completely changed script.js to make code more readable and folloq the DRY principal
    
    * Finished with the game! Looking forward to implement minimax

--- script.js
+++ script.js
@@ -1,146 +1,120 @@
-const gameBoard = document.querySelectorAll(".cell");
-const cells = Object.keys(gameBoard);
+let gameBoard = document.querySelectorAll(".cell");
 const resetBtn = document.querySelector("#reset");
-resetBtn.addEventListener("click", () => {
-  resetBoard();
-});
-const winCombos = [
-  [0, 1, 2],
-  [3, 4, 5],
-  [6, 7, 8],
-  //second combo
-  [0, 3, 6],
-  [1, 4, 7],
-  [2, 5, 8],
-  //third combo
-  [0, 4, 8],
-  [2, 4, 6],
-];
-const playerOccupiedCells = [];
-const computerOccupiedCells = [];
-// Starting the game
-engineStart();
-//main function which calls other functions required for the game to work
-function engineStart() {
-  //all functions are called under userClick function for time being
-  //it may shift to engineStart in future
-  userClick();
+const humanPlayer = "X";
+const cpu = "O";
+//
+startEngine();
+resetBtn.addEventListener("click", () => boardReset(gameBoard));
+///main function
+function startEngine() {
+  humanPlayerClick();
 }
-///checking for userclicks
-//currently user will be X
-function userClick() {
+
+//looping through the board to find out unoccupied places
+//PS : board should always be an array of elements!
+console.log(unoccupiedSpaces());
+function unoccupiedSpaces() {
+  let spaces = [];
+  for (let i = 0; i < gameBoard.length; i++) {
+    if (gameBoard[i].innerHTML === "") {
+      spaces.push(Number(gameBoard[i].id));
+    }
+  }
+  return spaces;
+}
+
+//function to check who is winning
+function checkWhoWins(board, player) {
+  if (
+    board[0].innerHTML === player &&
+    board[1].innerHTML === player &&
+    board[2].innerHTML === player
+  ) {
+    alert(`${player} wins!`);
+    return { gamestate: "won", whoWon: player };
+  } else if (
+    board[3].innerHTML === player &&
+    board[4].innerHTML === player &&
+    board[5].innerHTML === player
+  ) {
+    alert(`${player} wins!`);
+    return { gamestate: "won", whoWon: player };
+  } else if (
+    board[6].innerHTML === player &&
+    board[7].innerHTML === player &&
+    board[8].innerHTML === player
+  ) {
+    alert(`${player} wins!`);
+    return { gamestate: "won", whoWon: player };
+  } else if (
+    board[0].innerHTML === player &&
+    board[3].innerHTML === player &&
+    board[6].innerHTML === player
+  ) {
+    alert(`${player} wins!`);
+    return { gamestate: "won", whoWon: player };
+  } else if (
+    board[1].innerHTML === player &&
+    board[4].innerHTML === player &&
+    board[7].innerHTML === player
+  ) {
+    alert(`${player} wins!`);
+    return { gamestate: "won", whoWon: player };
+  } else if (
+    board[2].innerHTML === player &&
+    board[5].innerHTML === player &&
+    board[8].innerHTML === player
+  ) {
+    alert(`${player} wins!`);
+    return { gamestate: "won", whoWon: player };
+  } else if (
+    board[0].innerHTML === player &&
+    board[4].innerHTML === player &&
+    board[8].innerHTML === player
+  ) {
+    alert(`${player} wins!`);
+    return { gamestate: "won", whoWon: player };
+  } else if (
+    board[2].innerHTML === player &&
+    board[4].innerHTML === player &&
+    board[6].innerHTML === player
+  ) {
+    setTimeout(alert(`${player} wins!`), 400);
+
+    return { gameOver: true, whoWon: player };
+  }
+}
+
+function humanPlayerClick() {
   for (let i = 0; i < gameBoard.length; i++) {
     gameBoard[i].addEventListener("click", () => {
       if (gameBoard[i].textContent !== "") {
-        alert("already occupied!");
+        alert("Space already occupied! Please play a valid move.");
+        return;
       } else {
         gameBoard[i].innerHTML = "X";
-        gameBoard[i].style.background = "#419902";
-        playerOccupiedCells.push(Number(gameBoard[i].id));
-
-        //calling computer function after user clicks
-        computer();
-        if (
-          computerOccupiedCells.length !== 9 &&
-          playerOccupiedCells.length !== 9
-        ) {
-          setTimeout(() => {
-            checkWinner();
-          }, 250);
-        }
+        gameBoard[i].style.background = "#28c202";
+        checkWhoWins(gameBoard, humanPlayer);
+        //calling computerTurn fuction just after user clicks the cell;
+        computerTurn();
+        return;
       }
     });
   }
 }
-function resetBoard() {
-  for (let i = 0; i < gameBoard.length; i++) {
-    gameBoard[i].innerHTML = "";
-    gameBoard[i].style.background = "black  ";
-  }
-}
-function computer() {
-  let unoccupied = [];
-
-  for (let a = 0; a < gameBoard.length; a++) {
-    if (gameBoard[a].innerHTML == "") {
-      unoccupied.push(Number(gameBoard[a].id));
-    }
-  }
-  for (let j = 0; j < unoccupied.length; j++) {
-    let randomNum =
-      unoccupied[
-        Math.abs(
-          Math.floor(Math.random() * unoccupied.length + unoccupied.length) -
-            unoccupied.length
-        )
-      ];
-    gameBoard[randomNum].innerHTML = "O";
-    gameBoard[randomNum].style.background = "red";
-    computerOccupiedCells.push(randomNum);
-
-    return;
-  }
+//computerTurn function makes ai play its move;
+function computerTurn() {
+  let spaces = unoccupiedSpaces();
+  let static = spaces[Math.floor(Math.random() * spaces.length)];
+  gameBoard[static].textContent = "O";
+  gameBoard[static].style.background = "#e30505";
+  checkWhoWins(gameBoard, cpu);
 }
 
-//check who wins
-function checkWinner() {
-  if (
-    (gameBoard[0].textContent === "X" &&
-      gameBoard[1].textContent === "X" &&
-      gameBoard[2].textContent === "X") ||
-    (gameBoard[3].textContent === "X" &&
-      gameBoard[4].textContent === "X" &&
-      gameBoard[5].textContent === "X") ||
-    (gameBoard[6].textContent === "X" &&
-      gameBoard[7].textContent === "X" &&
-      gameBoard[8].textContent === "X") ||
-    (gameBoard[0].textContent === "X" &&
-      gameBoard[3].textContent === "X" &&
-      gameBoard[6].textContent === "X") ||
-    (gameBoard[1].textContent === "X" &&
-      gameBoard[4].textContent === "X" &&
-      gameBoard[7].textContent === "X") ||
-    (gameBoard[2].textContent === "X" &&
-      gameBoard[5].textContent === "X" &&
-      gameBoard[8].textContent === "X") ||
-    (gameBoard[0].textContent === "X" &&
-      gameBoard[4].textContent === "X" &&
-      gameBoard[8].textContent === "X") ||
-    (gameBoard[2].textContent === "X" &&
-      gameBoard[4].textContent === "X" &&
-      gameBoard[6].textContent === "X")
-  ) {
-    alert("Player Wins!!");
-    resetBoard();
-    return;
-  } else if (
-    (gameBoard[0].textContent === "O" &&
-      gameBoard[1].textContent === "O" &&
-      gameBoard[2].textContent === "O") ||
-    (gameBoard[3].textContent === "O" &&
-      gameBoard[4].textContent === "O" &&
-      gameBoard[5].textContent === "O") ||
-    (gameBoard[6].textContent === "O" &&
-      gameBoard[7].textContent === "O" &&
-      gameBoard[8].textContent === "O") ||
-    (gameBoard[0].textContent === "O" &&
-      gameBoard[3].textContent === "O" &&
-      gameBoard[6].textContent === "O") ||
-    (gameBoard[1].textContent === "O" &&
-      gameBoard[4].textContent === "O" &&
-      gameBoard[7].textContent === "O") ||
-    (gameBoard[2].textContent === "O" &&
-      gameBoard[5].textContent === "O" &&
-      gameBoard[8].textContent === "O") ||
-    (gameBoard[0].textContent === "O" &&
-      gameBoard[4].textContent === "O" &&
-      gameBoard[8].textContent === "O") ||
-    (gameBoard[2].textContent === "O" &&
-      gameBoard[4].textContent === "O" &&
-      gameBoard[6].textContent === "O")
-  ) {
-    alert("Computer Wins!!");
-    resetBoard();
-    return;
-  }
+//reset the board after win and after draw or when the reset btn clicked
+function boardReset(board) {
+  for (let i = 0; i < board.length; i++) {
+    board[i].textContent = "";
+    board[i].style.background = "black";
+  } 
 }
